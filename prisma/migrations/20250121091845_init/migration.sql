859b9266a31ed606931fd8f4e35cd8b4d3f0fa55
Austin Ameh <arewageek@gmail.com>
2025-01-21 10:46:21 +0100

    Prisma Configuration

---
+++ prisma/migrations/20250121091845_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "chatId" TEXT NOT NULL,
+    "wallet" TEXT NOT NULL DEFAULT ''
+);
+
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "symbol" TEXT NOT NULL,
+    "supply" TEXT NOT NULL,
+    "description" TEXT NOT NULL DEFAULT '',
+    "creatorId" INTEGER NOT NULL,
+    CONSTRAINT "Token_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_chatId_key" ON "User"("chatId");
